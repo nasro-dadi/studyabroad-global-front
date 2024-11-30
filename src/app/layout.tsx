d020112ee1d868901cfd687c5724aa3ef3954456
OUALI NASREDDINE <55834100+nasro-dadi@users.noreply.github.com>
2024-12-01 00:04:16 +0100

    feat:Wrap ThemeProvider with Providers component for improved context management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
+
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import theme from "../theme";
 import "./globals.css";
+import Providers from "./providers";
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
@@ -26,7 +28,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.variable}>
         <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          <Providers>
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          </Providers>
         </AppRouterCacheProvider>
       </body>
     </html>

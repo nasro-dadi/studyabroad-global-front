ab4a032f61fba941394bfcbccc022038a9d1d685
OUALI NASREDDINE <55834100+nasro-dadi@users.noreply.github.com>
2024-12-01 00:06:25 +0100

    feat:Integrate Main component into Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Header from "./header";
+import Main from "./main";
 
 export default function Home() {
   return (
     <div className="container  mx-auto">
-      <header className="">
+      <header className=" px-[24px]">
         <Header />
       </header>
-      <main className=""></main>
-      <footer className=""></footer>
+      <main className="pt-[41px]">
+        <Main />
+      </main>
+      <footer></footer>
     </div>
   );
 }

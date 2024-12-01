2fc0e450c2d13e8e08cf9495fead53183fd79c07
OUALI NASREDDINE <55834100+nasro-dadi@users.noreply.github.com>
2024-12-01 11:08:25 +0100

    feat: Enhance table pagination styles for better responsiveness on small screens

--- src/components/ui/table-pagination/table-pagination-styles.ts
+++ src/components/ui/table-pagination/table-pagination-styles.ts
@@ -1,7 +1,14 @@
 import { TablePagination, styled } from "@mui/material";
 
-export const StyledTablePagination = styled(TablePagination)({
+export const StyledTablePagination = styled(TablePagination)(({ theme }) => ({
   "& .MuiTablePagination-selectLabel": {
     color: "grey",
   },
-});
+
+  [theme.breakpoints.down("sm")]: {
+    "& .MuiTablePagination-toolbar": {
+      paddingTop: theme.spacing(2),
+      flexDirection: "column",
+    },
+  },
+}));
